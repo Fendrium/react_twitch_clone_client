3ed1944d3ee9f55d44fc11f7c774b66477c88786
Charlie NVP <charlie.nguyenvanphuc@gmail.com>
2021-04-12 14:09:13 +0900

    Create action to connect to api

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
   renderError({ error, touched }) {
@@ -24,7 +26,7 @@ class StreamCreate extends Component {
   };
 
   onSubmit(formValues) {
-    console.log(formValues);
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -56,7 +58,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'StreamCreate',
   validate: validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
